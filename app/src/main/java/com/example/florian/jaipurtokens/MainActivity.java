50cb798a58c1275beb6692810fd0d7b558326bb9
Florian Beaufay <florian.beaufay@gmail.com>
2017-08-20 23:07:47 +0200

    Switch to real fullscreen

--- app/src/main/java/com/example/florian/jaipurtokens/MainActivity.java
+++ app/src/main/java/com/example/florian/jaipurtokens/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.florian.jaipurtokens;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
@@ -33,7 +34,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
 
         final TokenStackViewList tsvl = new TokenStackViewList();
 
